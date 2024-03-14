981c785b43383e9859474af4efefff0d6fad4ecb
elias derbew <eliasderbew1@gmail.com>
2024-03-14 15:49:23 +0300

    test

--- Movies-Recommendation-App/src/Components/About.jsx
+++ Movies-Recommendation-App/src/Components/About.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='flex justify-center'>
+    <div className='flex flex-col justify-center'>
         <h1 className='font-serif font-bold text-4xl m-11 text-white'> About Us Page</h1>
+
+        <p className='font-serif font-bold text-white text-xl m-11'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum temporibus fuga voluptatem enim, ducimus eligendi repudiandae velit pariatur! Odit dicta molestiae esse autem quidem eaque magni ipsa voluptas vel est?</p>
     </div>
   )
 }
