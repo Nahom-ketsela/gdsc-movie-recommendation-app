4f0613aadf6332ebf57b3aabfb54180e5707dfb0
elias derbew <eliasderbew1@gmail.com>
2024-03-14 15:51:27 +0300

    test 2

--- Movies-Recommendation-App/src/Components/About.jsx
+++ Movies-Recommendation-App/src/Components/About.jsx
@@ -6,6 +6,8 @@ function About() {
         <h1 className='font-serif font-bold text-4xl m-11 text-white'> About Us Page</h1>
 
         <p className='font-serif font-bold text-white text-xl m-11'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum temporibus fuga voluptatem enim, ducimus eligendi repudiandae velit pariatur! Odit dicta molestiae esse autem quidem eaque magni ipsa voluptas vel est?</p>
+
+        <p className='font-serif font-bold text-xl text-white m-11'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error in dolore, ipsum corporis placeat nostrum, nobis quibusdam praesentium iure labore aliquid iusto rem illo vel voluptas. Corrupti esse at magni.</p>
     </div>
   )
 }
