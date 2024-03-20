ff51b5903c556c12b497e4180b27546cb4d9ee26
Hayati21 <mailtohayathussen@gmail.com>
2024-03-20 13:54:54 +0300

    updated

--- Movies-Recommendation-App/src/Components/FavoriteContext.jsx
+++ Movies-Recommendation-App/src/Components/FavoriteContext.jsx
@@ -2,7 +2,6 @@ import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const FavoriteContext = createContext();
-
 export const FavoriteProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
 
